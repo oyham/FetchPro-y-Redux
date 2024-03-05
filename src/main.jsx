043e5ebdeeb890ea6f7c50e5556c986d71c356a7
Oyham <voyhamburu@gmail.com>
2024-03-05 06:54:10 -0300

    update readme

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-// 1. Usar Redux y
-// 1. hacer una petición GET a https://jsonplaceholder.typicode.com/users/1 y
-// guardar los datos en la Store.
-// 2. Mostrar el nombre de usuario y el username en un componente llamado Header
-// recogiendo los datos de la store.
-// 3. Crear un componente Email que muestre el email del usuario en un campo de
-// texto y que permita modificarlo.
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
